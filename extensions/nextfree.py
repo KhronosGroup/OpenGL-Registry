d0cef0165ae1a602913b01eb437bf731989ff137
Jon Leech <oddhack@sonic.net>
2017-01-21 20:45:45 -0800

    Add nextfree.py script to determine the next unused extension numbers for ARB, GL, and ES extensions. Add missing copyright statements to scripts.

---
+++ extensions/nextfree.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# nextfree.py - determine the next unused extension numbers.
+# Use this when registering a new ARB, vendor, and/or OpenGL ES extension.
+#
+# Use: nextfree.py
+
+import copy, os, re, string, sys
+
+def write(*args, **kwargs):
+    file = kwargs.pop('file', sys.stdout)
+    end = kwargs.pop('end', '\n')
+    file.write(' '.join([str(arg) for arg in args]))
+    file.write(end)
+
+# Load the registry
+file = 'registry.py'
+exec(open(file).read())
+
+# Track each number separately
+keys = { 'arbnumber', 'number', 'esnumber' }
+max = {}
+for k in keys:
+    max[k] = 0
+
+# Loop over all extensions updating the max value
+for name,v in registry.items():
+    for k in keys:
+        if k in v.keys():
+            n = v[k]
+            if (n > max[k]):
+                max[k] = n
+
+# Report next free values
+for k in keys:
+    write('Next free', k, '=', max[k] + 1)
--- extensions/printreg.py
+++ extensions/printreg.py
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2017 The Khronos Group Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# printreg(reg, varname)
+# Prints a registry Python data structure (see registry.py) in a consistent
+# fashion.
+
 def tab():
     return '        '
 
@@ -32,6 +51,9 @@ def printreg(reg, varname):
     print(varname, '= {')
 
     # print('keys in registry =', len(reg.keys()))
+
+    print('# OpenGL extension number and name registry')
+    print('')
 
     for key in sorted(reg.keys(), key = striplibs):
         ext = reg[key]
