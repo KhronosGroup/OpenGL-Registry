0dc24166d162723781f1bf9fe433f71fa03a7aa0
Jon Leech <oddhack@sonic.net>
2020-11-19 00:31:53 -0800

    Regenerate headers after !436

--- api/GLES/glext.h
+++ api/GLES/glext.h
@@ -19,7 +19,7 @@ extern "C" {
 #define GL_APIENTRYP GL_APIENTRY*
 #endif
 
-/* Generated on date 20201027 */
+/* Generated on date 20201119 */
 
 /* Generated C header for:
  * API: gles1
--- api/GLES3/gl3.h
+++ api/GLES3/gl3.h
@@ -25,7 +25,7 @@ extern "C" {
 #define GL_GLES_PROTOTYPES 1
 #endif
 
-/* Generated on date 20201027 */
+/* Generated on date 20201119 */
 
 /* Generated C header for:
  * API: gles2
